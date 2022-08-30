97b864053284ab96630b5e47ed65674783cd9bd3
bizza <francesco.bizzarri@stud.unifi.it>
2022-08-30 16:58:03 +0200

    Implementation of training and prediction of the MLP

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import numpy as np
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
-from sklearn.preprocessing import RobustScaler
+from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
 import sys
+
 
 FILES = ["cifar-10-python/cifar-10-batches-py/data_batch_1",
          "cifar-10-python/cifar-10-batches-py/data_batch_2",
@@ -10,6 +14,10 @@ FILES = ["cifar-10-python/cifar-10-batches-py/data_batch_1",
          "cifar-10-python/cifar-10-batches-py/data_batch_4",
          "cifar-10-python/cifar-10-batches-py/data_batch_5"]
 
+CLASSES = ["Airplane", "Automobile", "Bird", "Cat", "Deer", "Dog", "Frog", "Horse", "Ship", "Truck"]
+
+
+##--------------DATA LOADING--------------##
 def unpickle(file):
     import pickle
     with open(file, 'rb') as fo:
@@ -42,10 +50,26 @@ def load_test_data():
     test_classes = np.array(test[b'labels'])
     return test_RGB_pixels, test_classes
 
+
+##--------------DATA PRE-PROCESSING--------------##
 def normalize_data(train_pixels, test_pixels):  # such that σ^2 = 1 and μ = 0
-    sc = RobustScaler()
+    sc = StandardScaler()
     scaler = sc.fit(train_pixels)
     train_pixels_scaled = scaler.transform(train_pixels)
     test_pixels_scaled = scaler.transform(test_pixels)
     return train_pixels_scaled, test_pixels_scaled
+
+
+##--------------TRAINING and PREDICTION--------------##
+def train_and_predict(n):   # n is the number of examples batches that we will use to train our MLP
+    train_RGB_pixels, train_classes = load_training_data(n)
+    test_RGB_pixels, test_classes = load_test_data()
+    x, y = normalize_data(train_RGB_pixels, test_RGB_pixels)
+    clf = MLPClassifier((50, 20), tol=0.000001, activation="relu", max_iter=10, solver="adam", n_iter_no_change=20, verbose=True)
+    print("START OF TRAINING")
+    clf.fit(x, train_classes)
+    print("END OF TRAINING\n")
+    predictions = clf.predict(y)
+    return predictions, test_classes, clf
+
 
